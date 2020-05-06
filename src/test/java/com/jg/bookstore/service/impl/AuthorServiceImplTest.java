7d90727e40b9fa5b86852c88db9b7994a226f73c
josephgrech <josephgrech90@gmail.com>
2020-05-06 10:09:35 +0200

    Added Unit Tests for AuthorServiceImpl.

--- src/test/java/com/jg/bookstore/service/impl/AuthorServiceImplTest.java
+++ src/test/java/com/jg/bookstore/service/impl/AuthorServiceImplTest.java
@@ -11,30 +11,28 @@ import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static com.jg.bookstore.domain.exception.ErrorCode.AUTHOR_NOT_FOUND;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class AuthorServiceImplTest {
 
-    private static UUID ID = UUID.randomUUID();
-    private static String FIRST_NAME = "First";
-    private static String LAST_NAME = "Last";
+    private static Author AUTHOR = new Author();
+    private static UUID ID = AUTHOR.getId();
+    private static String FIRST_NAME = "First Name";
+    private static String LAST_NAME = "Last Name";
 
     @Mock
     private AuthorRepository authorRepository;
 
     @InjectMocks
     private AuthorServiceImpl authorService;
-
-    @Mock
-    private Author author;
 
     @BeforeEach
     public void beforeEach() {
@@ -43,23 +41,23 @@ public class AuthorServiceImplTest {
                 return invocation.getArguments()[0];
             }
         });
-        when(authorRepository.findByIdAndDeletedFalse(any(UUID.class))).thenReturn(Optional.of(author));
+        when(authorRepository.findByIdAndDeletedFalse(any(UUID.class))).thenReturn(Optional.of(AUTHOR));
 
-        when(author.getFirstName()).thenReturn(FIRST_NAME);
-        when(author.getLastName()).thenReturn(LAST_NAME);
-        when(author.getId()).thenReturn(ID);
+        AUTHOR.setFirstName(FIRST_NAME);
+        AUTHOR.setLastName(LAST_NAME);
     }
 
     @Test
     public void createAuthor_validModel_shouldCreateAuthor() {
-        final Author result = authorService.createAuthor(this.author);
-        verify(authorRepository).save(author);
-        assertThat(result).isEqualTo(author);
+        final Author result = authorService.createAuthor(AUTHOR);
+        verify(authorRepository).save(AUTHOR);
+        assertThat(result).isEqualTo(AUTHOR);
     }
 
     @Test
     public void getAuthorById_authorExist_shouldReturnAuthor() {
-        assertThatCode(() -> authorService.getAuthorById(UUID.randomUUID())).doesNotThrowAnyException();
+        assertThatCode(() -> authorService.getAuthorById(ID)).doesNotThrowAnyException();
+        verify(authorRepository).findByIdAndDeletedFalse(ID);
     }
 
     @Test
@@ -69,25 +67,46 @@ public class AuthorServiceImplTest {
             .isEqualTo(new BaseException(AUTHOR_NOT_FOUND));
     }
 
-    /*
-    @Override
-    public Author getAuthorById(final UUID authorId) {
-        return null;
+    @Test
+    public void getAuthors_noAuthorsExist_shouldReturnEmptyList() {
+        when(authorRepository.findByDeletedFalse()).thenReturn(List.of());
+        final List<Author> result = authorService.getAuthors();
+        verify(authorRepository).findByDeletedFalse();
+        assertThat(result.size()).isEqualTo(0);
     }
 
-    @Override
-    public List<Author> getAuthors() {
-        return null;
+    @Test
+    public void getAuthors_oneAuthorsExist_shouldReturnListWithOne() {
+        when(authorRepository.findByDeletedFalse()).thenReturn(List.of(AUTHOR));
+        final List<Author> result = authorService.getAuthors();
+        verify(authorRepository).findByDeletedFalse();
+        assertThat(result.size()).isEqualTo(1);
     }
 
-    @Override
-    public Author updateAuthor(final UUID authorId, final Author newValues) {
-        return null;
+    @Test
+    public void updateAuthor_authorExist_shouldUpdateAuthor() {
+        final Author result = authorService.updateAuthor(ID, AUTHOR);
+        verify(authorRepository).findByIdAndDeletedFalse(ID);
+        verify(authorRepository).save(AUTHOR);
+        assertThat(result).isEqualTo(AUTHOR);
     }
 
-    @Override
-    public void deleteAuthor(final UUID authorId) {
-
+    @Test
+    public void updateAuthor_authorDoesNotExist_shouldThrowAuthorNotFoundException() {
+        when(authorRepository.findByIdAndDeletedFalse(any(UUID.class))).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> authorService.updateAuthor(ID, AUTHOR))
+                .isEqualTo(new BaseException(AUTHOR_NOT_FOUND));
+        verify(authorRepository).findByIdAndDeletedFalse(ID);
+        verify(authorRepository, never()).save(AUTHOR);
     }
-     */
+
+    @Test
+    public void deleteAuthor_idProvided_shouldDelete() {
+        assertThat(AUTHOR.isDeleted()).isFalse();
+        assertThatCode(() -> authorService.deleteAuthor(ID)).doesNotThrowAnyException();
+        verify(authorRepository).findByIdAndDeletedFalse(ID);
+        verify(authorRepository).save(AUTHOR);
+        assertThat(AUTHOR.isDeleted()).isTrue();
+    }
+
 }
