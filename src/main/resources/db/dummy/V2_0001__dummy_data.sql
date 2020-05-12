2bc0ed29c7e1ad0e8f66f4c323487e7f23ae4cd3
josephgrech <josephgrech90@gmail.com>
2020-05-12 20:03:25 +0200

    Added schema and dummy data for client, account, user_detail, address, permission, permission_groups, account_permissions, account_permission_groups, and permission_group_permissions.

--- src/main/resources/db/dummy/V2_0001__dummy_data.sql
+++ src/main/resources/db/dummy/V2_0001__dummy_data.sql
@@ -1,8 +1,10 @@
 -- clients
-insert into bookstore.client values ('c530b46c-93b4-11ea-bb37-0242ac130002', 'client', 'secret');
+insert into bookstore.client_detail values ('c530b46c-93b4-11ea-bb37-0242ac130002', 'client', 'secret');
 
 -- account
-insert into bookstore.account values ('c530b818-93b4-11ea-bb37-0242ac130002', 'admin@mail.com', '123456', 'ACTIVE');
+insert into bookstore.account_detail values ('c530b818-93b4-11ea-bb37-0242ac130002', 'admin@mail.com', '123456', 'ACTIVE');
+insert into bookstore.account_detail values ('133ea938-9479-11ea-bb37-0242ac130002', 'reader@mail.com', '123456', 'ACTIVE');
+insert into bookstore.account_detail values ('133eaa1e-9479-11ea-bb37-0242ac130002', 'writer@mail.com', '123456', 'ACTIVE');
 
 -- user_detail
 insert into bookstore.user_detail values('c530b91c-93b4-11ea-bb37-0242ac130002', 'c530b818-93b4-11ea-bb37-0242ac130002', 'John', 'Doe', '+35679797979');
@@ -17,23 +19,47 @@ insert into bookstore.permission_group values ('d430c7e6-9476-11ea-bb37-0242ac13
 
 -- permission
 insert into bookstore.permission values ('d430ca48-9476-11ea-bb37-0242ac130002', 'ADMIN', 'Administrator permission.');
+
 insert into bookstore.permission values ('c530be4e-93b4-11ea-bb37-0242ac130002', 'GET_AUTHOR', 'Get Authors permission.');
 insert into bookstore.permission values ('c530bf16-93b4-11ea-bb37-0242ac130002', 'CREATE_AUTHOR', 'Create Author permission.');
 insert into bookstore.permission values ('c530bfd4-93b4-11ea-bb37-0242ac130002', 'UPDATE_AUTHOR', 'Update Author permission.');
 insert into bookstore.permission values ('d430c5d4-9476-11ea-bb37-0242ac130002', 'DELETE_AUTHOR', 'Delete Author permission.');
 
+insert into bookstore.permission values ('133e9204-9479-11ea-bb37-0242ac130002', 'GET_BOOK', 'Get Books permission.');
+insert into bookstore.permission values ('133e9420-9479-11ea-bb37-0242ac130002', 'CREATE_BOOK', 'Create Book permission.');
+insert into bookstore.permission values ('133e9524-9479-11ea-bb37-0242ac130002', 'UPDATE_BOOK', 'Update Book permission.');
+insert into bookstore.permission values ('133e9696-9479-11ea-bb37-0242ac130002', 'DELETE_BOOK', 'Delete Book permission.');
+
+insert into bookstore.permission values ('133e995c-9479-11ea-bb37-0242ac130002', 'GET_ORDER', 'Get Order permission.');
+insert into bookstore.permission values ('133e9a42-9479-11ea-bb37-0242ac130002', 'CREATE_ORDER', 'Create Order permission.');
+insert into bookstore.permission values ('133ea78a-9479-11ea-bb37-0242ac130002', 'UPDATE_ORDER', 'Update Order permission.');
+
 -- permission_group_permissions
-insert into bookstore.permission_group_permissions values ('c530bc46-93b4-11ea-bb37-0242ac130002', 'c530be4e-93b4-11ea-bb37-0242ac130002');
-insert into bookstore.permission_group_permissions values ('d430c7e6-9476-11ea-bb37-0242ac130002', 'c530bf16-93b4-11ea-bb37-0242ac130002');
-insert into bookstore.permission_group_permissions values ('d430c7e6-9476-11ea-bb37-0242ac130002', 'c530bfd4-93b4-11ea-bb37-0242ac130002');
-insert into bookstore.permission_group_permissions values ('d430c7e6-9476-11ea-bb37-0242ac130002', 'd430c5d4-9476-11ea-bb37-0242ac130002');
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Read Group'), (select id from bookstore.permission where name='GET_AUTHOR'));
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Read Group'), (select id from bookstore.permission where name='GET_BOOK'));
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Read Group'), (select id from bookstore.permission where name='GET_ORDER'));
+
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Write Group'), (select id from bookstore.permission where name='CREATE_AUTHOR'));
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Write Group'), (select id from bookstore.permission where name='UPDATE_AUTHOR'));
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Write Group'), (select id from bookstore.permission where name='DELETE_AUTHOR'));
+
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Write Group'), (select id from bookstore.permission where name='CREATE_BOOK'));
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Write Group'), (select id from bookstore.permission where name='UPDATE_BOOK'));
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Write Group'), (select id from bookstore.permission where name='DELETE_BOOK'));
+
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Write Group'), (select id from bookstore.permission where name='CREATE_ORDER'));
+insert into bookstore.permission_group_permissions values ((select id from bookstore.permission_group where name='Individual Write Group'), (select id from bookstore.permission where name='UPDATE_ORDER'));
 
 -- account_permissions
-insert into bookstore.account_permissions values ('c530b818-93b4-11ea-bb37-0242ac130002', 'd430ca48-9476-11ea-bb37-0242ac130002');
+insert into bookstore.account_permissions values ('c530b818-93b4-11ea-bb37-0242ac130002', (select id from bookstore.permission where name='ADMIN'));
 
 -- account_permission_groups
-insert into bookstore.account_permission_groups values ('c530b818-93b4-11ea-bb37-0242ac130002', 'c530bc46-93b4-11ea-bb37-0242ac130002');
-insert into bookstore.account_permission_groups values ('c530b818-93b4-11ea-bb37-0242ac130002', 'd430c7e6-9476-11ea-bb37-0242ac130002');
+insert into bookstore.account_permission_groups values ('c530b818-93b4-11ea-bb37-0242ac130002', (select id from bookstore.permission_group where name='Individual Read Group'));
+insert into bookstore.account_permission_groups values ('c530b818-93b4-11ea-bb37-0242ac130002', (select id from bookstore.permission_group where name='Individual Write Group'));
+
+insert into bookstore.account_permission_groups values ('133ea938-9479-11ea-bb37-0242ac130002', (select id from bookstore.permission_group where name='Individual Read Group'));
+
+insert into bookstore.account_permission_groups values ('133eaa1e-9479-11ea-bb37-0242ac130002', (select id from bookstore.permission_group where name='Individual Write Group'));
 
 -- authors
 insert into bookstore.author values ('682abafb-ca77-4509-a2f0-157ff22bae70', false, 'Joseph', 'Grech');
